2b6bd93ed2381f029ad209e408c689ec3ac41dc8
sanikadeore <sanikadeore2005@gmail.com>
2025-01-24 22:09:44 +0530

    Create StudentGradeTracker.java

---
+++ StudentGradeTracker.java
@@ -0,0 +1,62 @@
+package com.mycompany.codealpha;
+
+import java.util.*;
+import java.util.Scanner;
+
+public class StudentGradeTracker {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> grades = new ArrayList<>();
+
+        System.out.print("Enter the number of students: ");
+        int numStudents = scanner.nextInt();
+
+        for (int i = 0; i < numStudents; i++) {
+            System.out.print("Enter grade for student " + (i + 1) + ": ");
+            int grade = scanner.nextInt();
+            grades.add(grade);
+        }
+
+        double average = calculateAverage(grades);
+        int highest = findHighest(grades);
+        int lowest = findLowest(grades);
+
+        System.out.println("\nResults:");
+        System.out.println("Average grade: " + average);
+        System.out.println("Highest grade: " + highest);
+        System.out.println("Lowest grade: " + lowest);
+
+        scanner.close();
+    }
+
+    public static double calculateAverage(ArrayList<Integer> grades) {
+        int sum = 0;
+        for (int grade : grades) {
+            sum += grade;
+        }
+        return (double) sum / grades.size();
+    }
+
+    public static int findHighest(ArrayList<Integer> grades) {
+        int highest = grades.get(0);
+        for (int grade : grades) {
+            if (grade > highest) {
+                highest = grade;
+            }
+        }
+        return highest;
+    }
+
+    public static int findLowest(ArrayList<Integer> grades) {
+        int lowest = grades.get(0);
+        for (int grade : grades) {
+            if (grade < lowest) {
+                lowest = grade;
+            }
+        }
+        return lowest;
+    }
+}
